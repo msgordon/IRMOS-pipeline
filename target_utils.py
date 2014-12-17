1d357c132886ec5b2f508fbe76ea2c2c8d7f6ed5
Michael Gordon <msgordon.astro@gmail.com>
2014-12-17 13:35:58 -0600

    added aperture extraction to target_utils.py

--- target_utils.py
+++ target_utils.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import numpy as np
+import pyfits
 
 def targets_from_region(filename):
     '''Read target stripes from region file.'''
@@ -20,3 +21,18 @@ def targets_from_region(filename):
         targets_t.append((targets[i-1],targets[i]))
         
     return targets_t
+
+
+def apertures_from_region(data, regfile):
+    '''Return aperture slices from data.'''
+    if isinstance(data,str):
+        data = pyfits.getdata(data)
+
+    targets = targets_from_region(regfile)
+
+    apertures = []
+    for target in targets:
+        aperture = data[target[1]:target[0],:]
+        apertures.append(aperture)
+
+    return apertures
