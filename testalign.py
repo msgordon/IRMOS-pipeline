bf78cfdd364355841c570bba119916fddda1ee61
Michael S Gordon <gordon@astro.umn.edu>
2015-02-11 15:33:15 -0600

    updated testalign.py

--- testalign.py
+++ testalign.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import pyfits
+from scipy.interpolate import interp1d
 
 def get_spectrum(filename):
     data, header = pyfits.getdata(filename, header=True)
@@ -11,11 +12,32 @@ def get_spectrum(filename):
     if len(data) != int(header['NAXIS1']):
         data = data[0]
 
-    minwave = header['CRVAL1']
-    maxwave = len(data)*header['CDELT1']+minwave
-    waves = np.linspace(minwave, maxwave, num=len(data))
+    start = header['CRVAL1']
+    step = header['CDELT1']
+    waves = np.arange(0,len(data))*step+start
+    #waves = np.linspace(minwave, maxwave, num=len(data))
 
     return (waves, data)
+
+def rescale(spectrum,minwave,maxwave):
+    # newx same length as originally spectrum, but rescaled
+    newx = np.linspace(minwave,maxwave,num = len(spectrum[0]))
+
+    fy = interp1d(spectrum[0],spectrum[1], kind='linear',bounds_error=False)
+
+    #apply interp function to newx
+
+    newy = fy(newx)
+
+    return (newx,newy)
+
+
+def combine_sky(spectra):
+    ys = np.array([spec[1] for spec in spectra])
+    y = np.nanmean(ys,axis=0)
+
+    #all xs are the same, so choose the first
+    return (spectra[0][0], y)
     
 
 def main():
@@ -28,14 +50,25 @@ def main():
 
     spectra = [get_spectrum(filename) for filename in args.filelist]
 
+    ## plot all together and offset to test alignment
     for idx, spectrum in enumerate(spectra):
         waves, data = spectrum
         plt.plot(waves,data+idx)
 
-    min_wave = np.max([np.min(x[0]) for x in spectra])
-    max_wave = np.min([np.max(x[0]) for x in spectra])
+    #find bounds
+    min_wave = np.min([np.min(x[0]) for x in spectra])
+    max_wave = np.max([np.max(x[0]) for x in spectra])
 
-    plt.vlines([min_wave, max_wave], 0, len(spectra), linewidth=5)
+    # plot bounds
+    #plt.vlines([min_wave, max_wave], 0, len(spectra), linewidth=5)
+    
+    newspectra = [rescale(spec,min_wave,max_wave) for spec in spectra]
+
+    # newspectra should all be same length, same waves
+    skyspec = combine_sky(newspectra)
+
+    plt.figure()
+    plt.plot(skyspec[0],skyspec[1])
     
     plt.show()
 
