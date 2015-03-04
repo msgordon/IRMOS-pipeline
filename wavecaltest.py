48e0083b46c63552da6402342d85471fda71e3d3
Travis Graham <tgraham@waziyata.spa.umn.edu>
2015-03-04 15:06:11 -0600

    Added a Legendre/Chebyshev test to wavecalest.

--- wavecaltest.py
+++ wavecaltest.py
@@ -2,20 +2,31 @@
 
 import pyfits
 import numpy as np
+import matplotlib.pyplot as plt
 
-data=pyfits.getdata('NGC253.ms.fits')
+'''data=pyfits.getdata('NGC253.ms.fits')
 aperture=data[0]
-header=pyfits.getheader('NGC253.ms.fits')
+header=pyfits.getheader('NGC253.ms.fits')'''
 
-x=np.array([280,307,336,344,352,369,387,473,505,522,541,562])
-y=np.array([19771.8,20008.2,20275.9,20339.5,20412.7,20563.6,20729,21505.5,21802.2,21955.6,22125.5,22312.7])
+x=np.array([221,247,275,290,308,325,442,459,477,498])
+y=np.array([19771.80,20008.20,20275.90,20412.70,20563.60,20729.00,21802.20,21955.60,22125.50,22312.70])
 
-z=np.polyfit(x,y,1)
-p=np.poly1d(z)
+#z=np.polyfit(x,y,1)
+#p=np.poly1d(z)
 
-header['CDELT1']=z[0]
+'''header['CDELT1']=z[0]
 header['CRPIX1']=1
-header['CRVAL1']=p(1)
+header['CRVAL1']=p(1)'''
 
-print 'Writing .fits file for the spectrum'
-pyfits.writeto('NGC253_s1.fits', aperture, header)
+xfit=np.arange(np.min(x),np.max(x)+200,1)
+c=np.polynomial.legendre.legfit(x,y,3)
+yfit=np.polynomial.legendre.legval(xfit,c)
+#xfit=np.arange(np.min(x),np.max(x),1)
+#yfit=np.array(p(xfit))
+
+plt.plot(x,y,'b-')
+plt.plot(xfit,yfit,'r-')
+plt.show()
+
+'''print 'Writing .fits file for the spectrum'
+pyfits.writeto('NGC253_s1.fits', aperture, header)'''
