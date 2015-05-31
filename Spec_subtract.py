4a19143c1b8b742794235c96207d131fdf0bfe4f
Travis Graham <tgraham@keewatin.spa.umn.edu>
2015-05-31 18:00:12 -0500

    Wrote Spec_subtract.py for checking the rms of two subtracted spectra.

---
+++ Spec_subtract.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import pyfits
+from scipy.interpolate import interp1d
+from math import sqrt
+
+def rescale(x,y,minwave,maxwave):
+    # newx same length as original spectrum, but rescaled
+    newx = np.linspace(minwave,maxwave,num = len(x))
+    
+    fy = interp1d(x,y, kind='slinear',bounds_error=False)
+    
+    #apply interp function to newx
+    newy = fy(newx)
+    
+    return (newx,newy)
+
+def main():
+    parser = argparse.ArgumentParser(description='Takes two spectra files and subtracts them, then gives the resulting RMS.')
+    parser.add_argument('specimgs',type=str,help='File location and handle for the spectra (e.g. NGC253_spec).')
+    parser.add_argument('spec1',type=int,help='Number of first aperture.')
+    parser.add_argument('spec2',type=int,help='Number of second aperture.')
+    parser.add_argument('wrange',nargs=2,type=int,help='Wavelength range in Angstroms (Enter 2 numbers, low and high).')
+    
+    args=parser.parse_args()
+    
+    ydata=[]
+    ydata.append(pyfits.getdata(args.specimgs+'%i.fits' % args.spec1))
+    ydata.append(pyfits.getdata(args.specimgs+'%i.fits' % args.spec2))
+    ydata=np.array(ydata)
+    
+    heads=[]
+    heads.append(pyfits.getheader(args.specimgs+'%i.fits' % args.spec1))
+    heads.append(pyfits.getheader(args.specimgs+'%i.fits' % args.spec2))
+    
+    xdata=[]
+    xdata.append(np.arange(0,len(ydata[0]))*heads[0]['CDELT1']+heads[0]['CRVAL1'])
+    xdata.append(np.arange(0,len(ydata[1]))*heads[1]['CDELT1']+heads[1]['CRVAL1'])
+    xdata=np.array(xdata)
+    
+    min_wave = np.min([np.min(x) for x in xdata])
+    max_wave = np.max([np.max(x) for x in xdata])
+    
+    newspecs = [rescale(xdata[i],ydata[i],min_wave,max_wave) for i,spec in enumerate(xdata)]
+    xnew=[]
+    ynew=[]
+    for spec in newspecs:
+        xnew.append(spec[0])
+        ynew.append(spec[1])
+    xnew=np.array(xnew)
+    ynew=np.array(ynew)
+    
+    ysub=ynew[0]-ynew[1]
+    yvals=np.array([ysub[i] for i,j in enumerate(xnew[0]) if j>=args.wrange[0] and j<=args.wrange[1]])
+    rms=sqrt(sum(n*n for n in yvals)/float(len(yvals)))
+    
+    print "RMS is: %f" % rms
+    
+    plt.plot(xnew[0], ysub)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
